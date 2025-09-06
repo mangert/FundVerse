6b9f4ab18aa39ac4dbd00bf32b0bdb4b4bc8716b
mangert <mangert@yandex.ru>
2025-09-07 00:32:51 +0300

    minor changes

--- front/src/config/wagmi.ts
+++ front/src/config/wagmi.ts
@@ -3,7 +3,7 @@ import { hardhat, sepolia } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
-export const chains = [hardhat, sepolia] as const
+export const chains = [/*hardhat, */sepolia] as const
 export const defaultChain = sepolia
 
 const walletConnectProjectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || '';
@@ -11,9 +11,9 @@ const walletConnectProjectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || 
 export const config = getDefaultConfig({
   appName: 'FundVerse',
   projectId: walletConnectProjectId,
-  chains: [hardhat, sepolia],
+  chains: [/*hardhat,*/ sepolia],
   transports: {
-    [hardhat.id]: http('http://127.0.0.1:8545'),
+    //[hardhat.id]: http('http://127.0.0.1:8545'), //убрала, потому что кидает ошибки на сервере
     [sepolia.id]: http(import.meta.env.VITE_ALCHEMY_API_KEY as string),
   },  
 });
